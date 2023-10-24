16ab2bf3ec53135d5d99344272764a2e0a6b8f11
MarcoNobody1 <marcocamaradiaz@gmail.com>
2023-10-24 12:18:36 +0200

    Se trata array de datos y se crea archivo countries csv mediante writefilesync

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,46 @@ import fs from "fs";
 
 const data = fs.readFileSync("countries.txt", "utf8");
 
+const separatedData: string[] = data.split("\n");
+
+const arrangedData = separatedData.map((country) =>
+  country.split(" ").reverse()
+);
+
+const filteredLocations = arrangedData.filter(country => isNaN(parseInt(country[2])) === true);
+
+const dataInObject = filteredLocations
+  .map((country) => {
+    const area = parseInt(country[0].replace(/,/g, ""));
+    const population = parseInt(country[1].replace(/,/g, ""));
+    const density = Math.round(population / area);
+    
+
+    return {
+      area: area < 10 ? undefined : area,
+      population: isNaN(population) ? undefined : population,
+      location: country.splice(2, country.length-1).reverse().join(" "),
+      density: density,
+    };
+  })
+  .filter(
+    (country) =>
+      country.area !== undefined &&
+      country.population !== undefined &&
+      country.density !== undefined
+  );
+
+
+
+const sortedCountries = dataInObject.sort((a, b) => b.density - a.density);
+
+console.log(sortedCountries)
+
+sortedCountries.map((country) => {
+  fs.writeFileSync("countries.csv", `${country.location},${country.population},${country.area},${country.density},`,{
+        encoding: "utf8",
+        flag: "a+",
+        mode: 0o666
+      }
+  );
+});
